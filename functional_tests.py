155bf10aa563a165685c3506e42aaf803109af2e
jakubbarak2001 <jakubbarak2001@seznam.cz>
2025-12-31 06:49:11 +0100

    First FT specced out in comments and now uses unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,30 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-assert "Congratulations!" in browser.title
-print("OK")
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_todo_list(self):
+        # Jacob has heard of a new cool on-line to do app.
+        # He opens his browser to check the app's homepage
+        self.browser.get("http://localhost:8000")
+
+        #He notices that the site shows 'To-Do' in its title
+        self.assertIn("To-Do", self.browser.title)
+
+        #He is invited to enter a to-do item straight away.
+        self.fail("Finish the test!")
+
+        #There is still a text box inviting him to write a new item
+        #He creates new item "Visit parents."
+
+        #The age updates again, now showing both of the items in the to-do list
+        #Satisfied, he goes to sleep
+
+if __name__ == '__main__':
+    unittest.main()
