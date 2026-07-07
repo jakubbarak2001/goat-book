0322df7ba724bcb11e66e285eb70d2aff5f14f90
jakubbarak2001 <jakubbarak2001@seznam.cz>
2026-07-07 13:14:49 +0200

    Add working .gitignore, untrack generated file, swapped tests.py test for a SmokeTest

--- lists/tests.py
+++ lists/tests.py
@@ -3,9 +3,6 @@ from django.http import HttpRequest
 from lists.views import home_page
 
 
-class HomePageTest(TestCase):
-    def test_home_page_returns_correct_html(self):
-        response = self.client.get("/")
-        self.assertContains(response, "<title>To-Do lists</title>")
-        self.assertContains(response, "<html>")
-        self.assertContains(response, "</html>")
+class SmokeTest(TestCase):
+    def test_bad_math(self):
+        self.assertEqual(1 + 1, 3)
