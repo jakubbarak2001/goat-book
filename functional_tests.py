900e7d44a2e937dc4dc4f6208503232b3e1991f0
jakubbarak2001 <jakubbarak2001@seznam.cz>
2026-07-07 19:05:35 +0200

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -35,7 +35,10 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element(By.ID, "id_list_table")
         rows = table.find_elements(By.TAG_NAME, "tr")
-        self.assertTrue(any(row.text == "1: study python" for row in rows))
+        self.assertTrue(
+         any(row.text == "1: study python" for row in rows),
+          "New to-do item did not appear in table",
+          )
 
         #There is still a text box inviting him to write a new item
         #He creates new item "Visit parents."
